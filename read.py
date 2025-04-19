ffc0354af76e084527e4b0dd452af24de90bc786
penguin-lola <leat1384@gmail.com>
2025-04-19 13:03:22 +0100

    Add files via upload

---
+++ read.py
@@ -0,0 +1,19 @@
+import pyautogui
+import time
+reroll = 638, 855
+exit = 1384, 200
+
+#screenshot = pyautogui.screenshot(region = (497, 587, 253, 225))
+#screenshot.save("screenshot.png")
+
+while True:
+    print(pyautogui.position())
+    time.sleep(3)
+
+#top left 501 643 left 500 687 right 746 683 bottem right 744 758
+
+region=(501, 643, 243, 115)
+
+region=(497, 587, 253, 225)
+
+
