b2f92318a55c276e4e25afae161a8ee1bf0cff49
penguin-lola <leat1384@gmail.com>
2025-04-19 14:44:40 +0100

    Update read.py

--- read.py
+++ read.py
@@ -1,26 +1,24 @@
 import pyautogui
 import time
+import keyboard
 #reroll = 638, 855
 #exit = 1384, 200
 
-#if you cant get it to work add my discord just2good4u.
-
-#make sure autoit is downloaded this is official site: https://www.autoitscript.com/cgi-bin/getfile.pl?autoit3/autoit-v3-setup.zip
-#once downloaded run this in terminal pip install pyautogui keyboard autoit pywin32
+#press f9 to print mouse position of reroll and exit button
 
 
+def print_mouse_position():
+    x, y = pyautogui.position()
+    print(f"Current mouse position: ({x}, {y})")
+keyboard.add_hotkey("f9", print_mouse_position)
 
-screenshot = pyautogui.screenshot(region = (497, 587, 253, 225))
-screenshot.save("screenshot.png")
+image = "reroll.png"
+image2 = "exit.png"
 
-while True:
-    print(pyautogui.position())
-    time.sleep(3)
+location = pyautogui.locateOnScreen(image, confidence=0.9)
+print(location)
 
-#top left 501 643 left 500 687 right 746 683 bottom right 744 758
+a = input("")
 
-#region=(501, 643, 243, 115)
-
-#region=(497, 587, 253, 225)
 
 
