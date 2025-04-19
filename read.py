8428f1a06b69cd42303ab848cb4b77803ab3860c
penguin-lola <leat1384@gmail.com>
2025-04-19 13:04:57 +0100

    read.py

--- read.py
+++ read.py
@@ -1,19 +1,22 @@
 import pyautogui
 import time
-reroll = 638, 855
-exit = 1384, 200
+#reroll = 638, 855
+#exit = 1384, 200
 
-#screenshot = pyautogui.screenshot(region = (497, 587, 253, 225))
-#screenshot.save("screenshot.png")
+#if you cant get it to work add my discord just2good4u.
+
+
+screenshot = pyautogui.screenshot(region = (497, 587, 253, 225))
+screenshot.save("screenshot.png")
 
 while True:
     print(pyautogui.position())
     time.sleep(3)
 
-#top left 501 643 left 500 687 right 746 683 bottem right 744 758
+#top left 501 643 left 500 687 right 746 683 bottom right 744 758
 
-region=(501, 643, 243, 115)
+#region=(501, 643, 243, 115)
 
-region=(497, 587, 253, 225)
+#region=(497, 587, 253, 225)
 
 
